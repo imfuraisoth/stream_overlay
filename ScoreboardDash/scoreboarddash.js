e1595f1be5e2cd54189266405d9a92110da1858e
Jason <imfuraisoth@yahoo.com>
2026-06-07 12:50:55 -0700

    Added play bracket info into json

--- ScoreboardDash/scoreboarddash.js
+++ ScoreboardDash/scoreboarddash.js
@@ -520,6 +520,8 @@ function resetNamesAndScore() {
 	jsonData.p2Score = "0";
 	jsonData.p1Seed = "";
 	jsonData.p2Seed = "";
+    jsonData.p1Bracket = "";
+    jsonData.p2Bracket = "";
 	updateCurrentPlayerDisplay();
 	sendJsonToEndpoint("updatealldata");
 }
@@ -619,6 +621,8 @@ function resetScores() {
 	document.getElementById("form_score_2p").value = "0";
 	jsonData.p1Score = "0";
 	jsonData.p2Score = "0";
+    jsonData.p1Bracket = "";
+    jsonData.p2Bracket = "";
 	updateCurrentPlayerDisplay();
 	sendJsonToEndpoint('updateCurrentScore');
 }
@@ -663,6 +667,8 @@ function resetAll() {
 	jsonData.resultplayer2 = "";
 	jsonData.resultscore1 = "";
 	jsonData.resultscore2 = "";
+	jsonData.p1Bracket = "";
+	jsonData.p2Bracket = "";
 	jsonData.maxScore = "";
 	jsonData.round = "Casuals";
 	jsonData.nextRound = "Casuals";
